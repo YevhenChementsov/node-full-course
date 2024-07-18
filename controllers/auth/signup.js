e16a34acc3dbd0415932a7ddf1b7a17a8e11b204
Yevhen Chementsov <yevhenchementsov@gmail.com>
2024-07-18 12:29:45 +0300

    Add custom message for 409 http error

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,6 +1,14 @@
+const { HttpError } = require('../../helpers');
 const { User } = require('../../models/user');
 
 const signUp = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (email) {
+    throw HttpError(409, 'User with this email already exists');
+  }
+
   const newUser = await User.create(req.body);
 
   res.status(201).json({
